4ded96a71b007892bfc15feef50798be8f026819
RJ <romain.jeanne@axa.com>
2023-01-11 17:10:04 +0100

    fix: pass username in package scan parameter

--- dispatcher.sh
+++ dispatcher.sh
@@ -5,10 +5,10 @@ repo_file_list='listRepos.csv'
 number_of_lines=$(wc -l < "$repo_file_list")
 while IFS="" read -r line || [ -n "$line" ]
 do
-	repo="$(basename "$line")"
-	printf "Analyzing %s (%d/%d)...\n\n" "$repo" "$counter" "$number_of_lines"
-	gh workflow run packagescan.yml --field repo="$repo" --field packagename="results-${repo}"
-	echo 'done'
-	#gh run watch
+	reponame="$(basename "${url%.*}")"
+	username="$(basename "${url%.*%/"${reponame}"}")"
+	printf "Analyzing %s packages (%d/%d)...\n\n" "$reponame" "$counter" "$number_of_lines"
+	gh workflow run packagescan.yml --field repo="$reponame/$username" --field packagename="${username}_$reponame"
+	gh run watch
 	counter=$((counter+1))
 done < "$repo_file_list"

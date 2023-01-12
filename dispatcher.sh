042b27ba34e624f4e6f7a2fe99624e3bab2e03f1
RJ <romain.jeanne@axa.com>
2023-01-12 14:16:28 +0100

    test: added trufflehog to CI

--- dispatcher.sh
+++ dispatcher.sh
@@ -2,13 +2,22 @@
 
 repo_file_list='listRepos.csv'
 
-scan_packages() {
+package_scan() {
 	echo "[*] - Starting package scan workflow on $repo_slash_user..."
 	gh workflow run packagescan.yml --field repo="$repo_slash_user" --field packagename="${username}_$reponame"
 	last_workflow_run_id="$(gh run list --workflow 'Package Scan' --json databaseId --jq '.[]| .databaseId' --limit 1)"
 	echo '[*] - Waiting for package scan workflow to finish...'
 	gh run watch --interval 1 --exit-status "$last_workflow_run_id" | grep 'a^'
 	echo '[+] - Package scan workflow exited'
+}
+
+secret_scan() {
+	echo "[*] - Starting secret scan workflow on $repo_slash_user..."
+	gh workflow run packagescan.yml --field repo="$repo_slash_user" --field packagename="${username}_$reponame"
+	last_workflow_run_id="$(gh run list --workflow 'Secret Scan' --json databaseId --jq '.[]| .databaseId' --limit 1)"
+	echo '[*] - Waiting for secret scan workflow to finish...'
+	gh run watch --interval 1 --exit-status "$last_workflow_run_id" | grep 'a^'
+	echo '[+] - Secret scan workflow exited'
 }
 
 # Fix for workflow queue
@@ -25,7 +34,8 @@ do
 	username="${repo_slash_user%/*}"
 	echo
 	echo "----- $repo_slash_user ($counter/$real_number_of_lines) -----"
-	scan_packages
+	package_scan
+	secret_scan
 	counter=$((counter+1))
 done < "$repo_file_list"
 echo '[+] - Done!'

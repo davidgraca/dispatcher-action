f3fed062a3013d6f29040a1d70419dd140ded916
RJ <romain.jeanne@axa.com>
2023-01-12 12:11:29 +0100

    test: scan packages in function

--- dispatcher.sh
+++ dispatcher.sh
@@ -2,6 +2,15 @@
 
 repo_file_list='listRepos.csv'
 
+scan_packages() {
+	echo "[*] - Starting package scan workflow on $repo_slash_user..."
+	gh workflow run packagescan.yml --field repo="$repo_slash_user" --field packagename="${username}_$reponame"
+	last_workflow_run_id="$(gh run list --workflow 'Package Scan' --json databaseId --jq '.[]| .databaseId' --limit 1)"
+	echo '[*] - Waiting for package scan workflow to finish...'
+	gh run watch --interval 1 --exit-status "$last_workflow_run_id" | grep 'a^'
+	echo '[+] - Package scan workflow exited'
+}
+
 # Fix for workflow queue
 gh run list --workflow 'dispatcher' --json databaseId --jq '.[]| .databaseId' --limit 1 | grep 'a^'
 
@@ -15,14 +24,7 @@ do
 	username="${repo_slash_user%/*}"
 	echo
 	echo "----- $repo_slash_user ($counter/$number_of_lines) -----"
-
-	echo "[*] - Starting package scan workflow on $repo_slash_user..."
-	gh workflow run packagescan.yml --field repo="$repo_slash_user" --field packagename="${username}_$reponame"
-	last_workflow_run_id="$(gh run list --workflow 'Package Scan' --json databaseId --jq '.[]| .databaseId' --limit 1)"
-	echo '[*] - Waiting for package scan workflow to finish...'
-	gh run watch --interval 1 --exit-status "$last_workflow_run_id" | grep 'a^'
-	echo '[+] - Package scan workflow exited'
-
+	scan_packages
 	counter=$((counter+1))
 done < "$repo_file_list"
 echo '[+] - Done!'

246f334de2b1f5871d8dca44d56151ccbadeeba4
RJ <romain.jeanne@axa.com>
2023-01-12 10:06:15 +0100

    fix: test repo name handling

--- dispatcher.sh
+++ dispatcher.sh
@@ -5,18 +5,22 @@ repo_file_list='listRepos.csv'
 package_scan() {
 	workflow_name='Package Scan'
 	echo 'Analyzing packages...'
-	gh workflow run packagescan.yml --field repo="$reponame/$username" --field packagename="test"
+	gh workflow run packagescan.yml --field repo="$repo_slash_user" --field packagename="${username}_$reponame"
 	last_workflow_run_id="$(gh run list --workflow "$workflow_name" --json databaseId --jq '.[]| .databaseId' --limit 1)"
-	#gh run watch --interval 1 --exit-status "$last_workflow_run_id" &> /dev/null
+	gh run watch --interval 1 --exit-status "$last_workflow_run_id" &> /dev/null
 }
+
+echo "${url#/*}"
 
 counter=1
 number_of_lines=$(wc -l < "$repo_file_list")
-while IFS="" read -r url || [ -n "$url" ]
+while IFS="" read -r line || [ -n "$line" ]
 do
-	reponame="$(basename "${url%.*}")"
-	username="$(basename "${url%.*%/"${reponame}"}")"
-	printf "Analyzing %s/%s (%d/%d)...\n" "$username" "$reponame" "$counter" "$number_of_lines"
+	url="${url%/}" # Remove trailing slash
+	reponame="${url##*/}"
+	repo_slash_user="${url%/*#*.*/}"
+	username="${repo_slash_user%/*}"
+	printf "Analyzing %s (%d/%d)...\n" "$repo_slash_user" "$counter" "$number_of_lines"
 	package_scan
 	counter=$((counter+1))
 done < "$repo_file_list"
